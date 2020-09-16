02415d21614fadbb50746b7038b80eb2b7528f29
Felipe <fezto.17@gmail.com>
2020-09-16 10:30:46 -0300

    finished routes

---
+++ src/routes/Route.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import {
+  Redirect,
+  Route as ReactDOMRoute,
+  RouteProps as ReactDOMRouteProps,
+} from 'react-router-dom';
+
+import { useAuth } from '../hooks/auth';
+
+interface RouteProps extends ReactDOMRouteProps {
+  isPrivate?: boolean;
+  component: React.ComponentType;
+}
+
+/* Logica de possiveis tipos de acessos */
+// (Uma rota privada / usuário autenticado)
+// true/true = OK
+// true/false = Redirecionar ele pro login
+// false/true = Redirecionar para o dashboard
+// false/false = OK
+
+const Route: React.FC<RouteProps> = ({
+  isPrivate = false,
+  component: Component,
+  ...rest
+}) => {
+  const { user } = useAuth();
+
+  return (
+    <ReactDOMRoute
+      {...rest}
+      render={({ location }) => {
+        return isPrivate === !!user ? (
+          <Component />
+        ) : (
+          <Redirect
+            to={{
+              pathname: isPrivate ? '/' : '/dashboard',
+              state: { from: location },
+            }}
+          />
+        );
+      }}
+    />
+  );
+};
+
+export default Route;
